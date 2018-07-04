7e2fb6e0fd725c127a13fd379832467309a0760a
yerasiashok <yerasiashokreddy@gmail.com>
2018-07-04 14:46:47 +0530

    updated

---
+++ sw.js
@@ -0,0 +1,93 @@
+
+/*self.addEventListener('install', function(event) {
+  event.waitUntil(
+    
+    caches.open('review5').then(function(cache) {
+      return cache.addAll([
+        '/',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg',
+      ]);
+    })
+  );
+});
+
+
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+
+*/
+
+var staticCacheName = 'review1-v2';
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(staticCacheName).then(function(cache) {
+      return cache.addAll([
+        '/',
+  './index.html',
+  './restaurant.html',
+  './css/styles.css',
+  './js/dbhelper.js',
+  './js/main.js',
+  './js/restaurant_info.js',
+  './data/restaurants.json',
+  './img/1.jpg',
+  './img/2.jpg',
+  './img/3.jpg',
+  './img/4.jpg',
+  './img/5.jpg',
+  './img/6.jpg',
+  './img/7.jpg',
+  './img/8.jpg',
+  './img/9.jpg',
+  './img/10.jpg'
+      ]);
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          return cacheName.startsWith('review1-') &&
+                 cacheName != staticCacheName;
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
+*/
